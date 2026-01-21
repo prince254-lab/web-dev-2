02f0b76072264668715ef48161a992a55c8704f5
prince254-lab <palv8235@gmail.com>
2026-01-21 15:44:06 +0530

    Add files via upload

---
+++ Day 3/javascript2.js
@@ -0,0 +1,12 @@
+// let sentence="i am a comma seperated sentence";
+// let words=sentence.split(" ");
+// console.log(words)
+
+// let sentence2="i am a comma seperated sentence";
+// let words2=sentence2.split("a");
+// console.log(words2)
+
+//Q1.find out the occurance of "r" in the given string
+let str="tu meri mai tera ,mai teri tu mera"
+let count=str.split("r").length-1;
+console.log("the occurance of 'r' in the given string is:"+count);
